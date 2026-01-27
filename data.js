66c5a834cef10a0e2581e217a2e20c193804a691
sumit12617-byte <sumit12617@gmail.com>
2026-01-27 18:16:28 +0530

    data.js

--- data.js
+++ data.js
@@ -6,7 +6,7 @@
 const students = [
     
     // 1. Copy the line below
-    { name: "Puneet Tiwari", username: "puneet426", thought: "Let's build something awesome!" },
+    { name: "Sumit Kumar", username: "sumit12617-cloud", thought: "Let's build something awesome!" },
     
 
     // 2. Paste it here and change the details
